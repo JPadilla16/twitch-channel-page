ea2037ffcd17e2c5799a138430c5c342aeff3443
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-14 09:54:48 -0600

    Added Code.

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 (!DOCTYPE html)
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Twitch Channel Page</title>
+	</head>
+	<body>
+		<header>
+		<h1>Data Design Project</h1>
+		</header>
+		<main>
+			<p>The typical persona for a Twitch channel is gamers.
+			The main use cases for a Twitch Channel is to stream your live gameplay for your audience.
+			The steps for Arriving to this Goal:
+				&bull; 
+				&bull;
+				&bull;</p>
+		</main>
+		<footer>
+			<div>&copy; 2016 JPadilla16</div>
+		</footer>
+	</body>
+</html>
