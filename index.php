485c671ad169bafd1d785e30a63173e6732ea726
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-14 10:25:16 -0600

    Added ordered and unordered list

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-(!DOCTYPE html)
+<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
@@ -9,12 +9,12 @@
 		<h1>Data Design Project</h1>
 		</header>
 		<main>
-			<p>The typical persona for a Twitch channel is gamers.
-			The main use cases for a Twitch Channel is to stream your live gameplay for your audience.
-			The steps for Arriving to this Goal:
-				&bull; 
-				&bull;
-				&bull;</p>
+			<h3>Frontend</h3>
+			<<ul><li>The typical persona for a Twitch channel is gamers.</li>
+			<li>The main use cases for a Twitch Channel is to stream your live gameplay for your audience.</li>
+			<li>The steps for Arriving to this Goal:</li>
+			</ul>
+			<h3>Backend</h3>
 		</main>
 		<footer>
 			<div>&copy; 2016 JPadilla16</div>
